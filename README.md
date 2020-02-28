a77f79cad54a5cfaa4aeb08033d376c514b0585e
Minh Pham <minhpham@mail.usf.edu>
2020-02-27 23:40:19 -0500

    Glue everything together

--- README.md
+++ README.md
@@ -6,3 +6,39 @@ Team leads: Rays Jiang and Minh Pham
 
 ## Simplified pipeline:
 ![simplified_pipeline](./simpified_pipeline.jpg)
+
+## INSTALLATION
+###	web server
+Install requirements:
+```
+sudo apt install python3 python3-pip
+pip3 install flask
+```
+Set up the web app service: 
+```
+sudo python3 ./server_setup.py
+sudo systemctl daemon-reload
+```
+
+Check if the app is running
+`systemctl status TRACK-flask.service`
+
+### search engine
+```
+cd search_engine
+wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.10.0+-x64-linux.tar.gz
+gunzip ncbi-blast-2.10.0+-x64-linux.tar.gz
+tar -xf ncbi-blast-2.10.0+-x64-linux.tar
+cp ./ncbi-blast-2.10.0+/bin/* ./
+```
+
+### database
+The database's FASTA file is static .Set up the database from FASTA file:
+```
+cd database 
+../search_engine/makeblastdb -in demo.fasta -blastdb_version 5 -title "TRACK data" -dbtype prot
+```
+To run search engine
+```
+./blastp -query query.faa -db demo.fasta -out blast_results.txt
+```
--- webserver/app.py
+++ webserver/app.py
@@ -1,12 +1,62 @@
-import os
-from flask import Flask, render_template
+import os, re
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['GET'])
 def index():
-    return render_template("index.html", message="Hello Flask!");   
+    return render_template("index.html", message="Hello Flask!")
+
+@app.route('/search', methods = ['POST'])
+def search():
+	search_sequences = request.form['squence']
+
+	# save search_sequences to a text file
+	f = open('./input.txt', 'w')
+	f.write(search_sequences)
+	f.close()
+	search_sequences = search_sequences.split('\r\n')
+
+	# CALL Desirae's code to put this into a FASTA FILE
+	os.system('Rscript ./search_engine/R_script_Desirae.R')
+
+	# RUN BLAST
+	os.system('./search_engine/blastp -threshold 14 -query queryout.txt -db ./database/demo.fasta -out blast_results.txt')
+
+	# parse output
+	outputs = parse_BLAST_output('blast_results.txt')
+	print(outputs)
+
+	return(render_template("output.html", outputs = outputs))
+
+	
+
+def parse_BLAST_output(filename):
+	start_recording = False
+	entry = '' 	 # will convert to html syntax
+	entries = [] # list of entry
+	for line in open(filename):
+		# find if this is start of recording 
+		if 'Query=' in line:
+			start_recording = True
+		# stop recording if hit the lambda line
+		if 'Lambda' in line:
+			start_recording = False
+		if start_recording:
+			if 'Query=' in line:
+				# append to output
+				if entry != '':
+					entries.append(entry)
+				# start a new entry
+				entry = '<pre>' + line.replace('\n', '') + '</pre>'  # for displaying in html
+			else:
+				# append to current entry
+				if line == '\n':
+					entry = entry + '<br>'
+				else:
+					entry = entry + '<pre>' + line.replace('\n', '') + '</pre>'
+	return entries
 
 
 if __name__ == "__main__":
